56d76c4ba5b203fc67499e5f41b7c137f72ddadc
tareqitos <contact@tareqitos.com>
2025-01-20 15:22:46 +0100

    Bug fixes and visual improvements

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,20 +1,13 @@
-import { useEffect, useState, useRef } from "react"
+import { useEffect, useRef } from "react"
 import { AtSymbolIcon, MoonIcon, SunIcon } from "@heroicons/react/24/outline";
-import { getTheme, toggleTheme } from '../theme/Theme.js';
 
 import ToTop from "../toTop";
 import styles from './Sidebar.module.scss';
-import header_styles from '../header/Header.module.scss';
+import links_styles from '../links/Links.module.scss';
 import components_styles from '../../styles/Components.module.scss'
 
 
-function Sidebar({ categories, sidebarActive, setSidebarActive, activeCategory }) {
-    const [theme, setTheme] = useState(getTheme());
-
-    function handleToggleTheme() {
-        toggleTheme();
-        setTheme(getTheme());
-    }
+function Sidebar({ categories, sidebarActive, setSidebarActive, activeCategory, setActiveCategory, theme, toggleTheme }) {
 
     useEffect(() => {
         function sidebarCloseOnResize() {
@@ -31,6 +24,38 @@ function Sidebar({ categories, sidebarActive, setSidebarActive, activeCategory }
         }
     }, [])
 
+    useEffect(() => {
+        const html = document.documentElement;
+        sidebarActive ? html.classList.add('stop-scrolling') : html.classList.remove('stop-scrolling')
+    }, [sidebarActive])
+
+       const observer = useRef(null);
+    
+        useEffect(() => {
+            const handleIntersect = (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        setActiveCategory(entry.target.id);
+                    }
+                });
+            };
+    
+            const options = {
+                root: null, // Use the viewport
+                rootMargin: '0px 0px -75% 0px', // Offset to trigger earlier
+                threshold: 0.6, // Trigger when 60% of the element is visible
+            };
+    
+            observer.current = new IntersectionObserver(handleIntersect, options);
+            const elements = document.querySelectorAll(`.${links_styles['anchor']}`);
+    
+            elements.forEach((element) => observer.current.observe(element));
+    
+            return () => {
+                elements.forEach((element) => observer.current.unobserve(element));
+            };
+        }, [setActiveCategory]);
+
     return (
         <aside className={`${styles['sidebar-main']} ${sidebarActive ? styles.active : ''}`} style={{ visibility: sidebarActive ? 'visible' : '' }}>
             <nav className={styles.navbar}>
@@ -46,8 +71,8 @@ function Sidebar({ categories, sidebarActive, setSidebarActive, activeCategory }
                 </ul>
                 <ToTop button_css_selector={styles['to-top-sidebar']} />
             </nav>
-            <div className={`${header_styles['header-right-side']} ${styles['sidebar-external-links']}`}>
-                <button onClick={() => handleToggleTheme()} className={`${components_styles.btn} ${styles['theme-toggle-sidebar']}`}>{theme == 'dark' ? <MoonIcon className={components_styles['h-icon']} /> : <SunIcon className={components_styles['h-icon']} />}</button>
+            <div className={` ${styles['sidebar-external-links']}`}>
+                <button onClick={() => toggleTheme()} className={`${styles['theme-toggle-sidebar']} ${components_styles.btn}`}>{theme == 'dark' ? <MoonIcon className={components_styles['h-icon']} /> : <SunIcon className={components_styles['h-icon']} />}</button>
 
                 <div className={`${components_styles.socials} ${styles['socials-sidebar']}`}>
                     <a href="https://github.com/tareqitos/yamedame.github.io" target="_blank"><i
