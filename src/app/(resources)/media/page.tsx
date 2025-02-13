3e4ea932af70273e3bd5dbc5e1850d12cc4ba192
tareqitos <contact@tareqitos.com>
2025-02-13 17:02:23 +0100

    Added sidebar and navbar component into pages

--- src/app/(resources)/media/page.tsx
+++ src/app/(resources)/media/page.tsx
@@ -6,6 +6,7 @@ import '@/styles/resources.scss'
 import Feedback from "@/app/components/feedback";
 import Image from "next/image";
 import Sidebar from "@/app/components/sidebar";
+import Navbar from "@/app/components/navbar";
 
 type Media = {
     id: number,
@@ -37,36 +38,39 @@ export default async function Media() {
     }
 
     return (
-        <div className="resources-wrapper">
-            <div className="resources-container">
-                <ScrollToHash />
-                <ResourcesTitle title="Media Library 💾" description="Teaching videos, vlogs, podcasts, gaming and more!" />
-                <Feedback
-                title="Share your favorite Youtube channel or podcast 📀"
-                categories={Object.keys(media)} />
-                <hr className="resources-title-separator" />
-                <div className="list-container media">
-                {Object.keys(media).map((category, i) => (
-                    <section key={i} className={`${media[category][0].slug}-container`}>
-                    <a id={`${media[category][0].slug}-id`} className="anchor"></a>
-                    <h2 className="category-title">{category}</h2>
-                    <ul key={i} className="list-item-container media">
-                        {media[category].map((item: Media) => (
-                        <li key={item.uuid} className={`item-container ${item.slug} media`}>
-                            <Image src={item.image} alt={item.name} width={60} height={60} className="item-picture media" />
-                            <div className="item-infos">
-                            <a href={item.link} className="item media" target="_blank">{item.name} <FontAwesomeIcon className="icon" icon={platforms_icons[item.platform]} width={20} /></a>
-                            {item.description}
-                            </div>
-                            <Image src={item.image} alt={item.name} width={100} height={100} className="item-picture-bg" />
-                        </li>
+        <>
+            <Navbar resources={media} />
+            <div className="resources-wrapper">
+                <div className="resources-container">
+                    <ScrollToHash />
+                    <ResourcesTitle title="Media Library 💾" description="Teaching videos, vlogs, podcasts, gaming and more!" />
+                    <Feedback
+                        title="Share your favorite Youtube channel or podcast 📀"
+                        categories={Object.keys(media)} />
+                    <hr className="resources-title-separator" />
+                    <div className="list-container media">
+                        {Object.keys(media).map((category, i) => (
+                            <section key={i} className={`${media[category][0].slug}-container`}>
+                                <a id={`${media[category][0].slug}-id`} className="anchor"></a>
+                                <h2 className="category-title">{category}</h2>
+                                <ul key={i} className="list-item-container media">
+                                    {media[category].map((item: Media) => (
+                                        <li key={item.uuid} className={`item-container ${item.slug} media`}>
+                                            <Image src={item.image} alt={item.name} width={60} height={60} className="item-picture media" />
+                                            <div className="item-infos">
+                                                <a href={item.link} className="item media" target="_blank">{item.name} <FontAwesomeIcon className="icon" icon={platforms_icons[item.platform]} width={20} /></a>
+                                                {item.description}
+                                            </div>
+                                            <Image src={item.image} alt={item.name} width={100} height={100} className="item-picture-bg" />
+                                        </li>
+                                    ))}
+                                </ul>
+                            </section>
                         ))}
-                    </ul>
-                    </section>
-                ))}
+                    </div>
                 </div>
+                <Sidebar resources={media} />
             </div>
-            <Sidebar resources={media} />
-        </div>
+        </>
     )
 }
--- src/app/(resources)/resources/page.tsx
+++ src/app/(resources)/resources/page.tsx
@@ -5,6 +5,7 @@ import ScrollToHash from "@/utils/scrollToHash";
 import '@/styles/resources.scss'
 import Feedback from "@/app/components/feedback";
 import Sidebar from "@/app/components/sidebar";
+import Navbar from "@/app/components/navbar";
 
 type Resource = {
     id: number,
@@ -19,7 +20,7 @@ type Resource = {
 
 
 export default async function Resources() {
-    
+
     const API_URL = process.env.NEXT_PUBLIC_API_URL;
     const response = await fetch(`${API_URL}/api/resources/resources`);
     if (!response.ok) {
@@ -39,33 +40,36 @@ export default async function Resources() {
     }
 
     return (
-        <div className="resources-wrapper">
-            <div className="resources-container">
-                <ScrollToHash />
-                <ResourcesTitle title="Study Resources 📖" description="Dictionaries, grammar guides, vocabulary insights, reading materials and other useful resources for studying!" />
-                <Feedback
-                    title="Share your favorite Japanese learning tool 📖"
-                    categories={Object.keys(resources)} />
-                <hr className="resources-title-separator" />
-                <div className="list-container">
-                    {Object.keys(resources).map((category, i) => (
-                        <section key={i} className={`${resources[category][0].slug}-container`}>
-                            <a id={`${resources[category][0].slug}-id`} className="anchor"></a>
-                            <h2 className="category-title">{category}</h2>
-                            <ul key={i} className="list-item-container">
-                                {resources[category].map((item: Resource) => (
-                                    <li key={item.uuid} className={`item-container ${item.slug}`}>
-                                        <FontAwesomeIcon className="item-icons" icon={category_icons[item.slug]} height={20} />
-                                        <a href={item.link} className="item" target="_blank">{item.name}</a>
-                                        {` - ${item.description}`}
-                                    </li>
-                                ))}
-                            </ul>
-                        </section>
-                    ))}
+        <>
+            <Navbar resources={resources} />
+            <div className="resources-wrapper">
+                <div className="resources-container">
+                    <ScrollToHash />
+                    <ResourcesTitle title="Study Resources 📖" description="Dictionaries, grammar guides, vocabulary insights, reading materials and other useful resources for studying!" />
+                    <Feedback
+                        title="Share your favorite Japanese learning tool 📖"
+                        categories={Object.keys(resources)} />
+                    <hr className="resources-title-separator" />
+                    <div className="list-container">
+                        {Object.keys(resources).map((category, i) => (
+                            <section key={i} className={`${resources[category][0].slug}-container`}>
+                                <a id={`${resources[category][0].slug}-id`} className="anchor"></a>
+                                <h2 className="category-title">{category}</h2>
+                                <ul key={i} className="list-item-container">
+                                    {resources[category].map((item: Resource) => (
+                                        <li key={item.uuid} className={`item-container ${item.slug}`}>
+                                            <FontAwesomeIcon className="item-icons" icon={category_icons[item.slug]} height={20} />
+                                            <a href={item.link} className="item" target="_blank">{item.name}</a>
+                                            {` - ${item.description}`}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </section>
+                        ))}
+                    </div>
                 </div>
+                <Sidebar resources={resources} />
             </div>
-            <Sidebar resources={resources} />
-        </div>
+        </>
     )
 }
