1c3a43df863a8a2f08b1265ba701b974de0c78a7
Tareqitos <contact@tareqitos.com>
2025-02-23 17:41:08 +0100

    Improved dashboard page

--- src/app/(dashboard)/dashboard/[id]/page.tsx
+++ src/app/(dashboard)/dashboard/[id]/page.tsx
@@ -1,34 +1,9 @@
-"use client"
+import VerifyAccountClient from "../dashboard"
 
-import { useAuth } from "@/context/authContext";
-import logout from "@/lib/logout"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react";
-
-export default function Dashboard() {
-    const {hasAccess, user, checkAccess} = useAuth();
-    const router = useRouter();
-
-    const handleLogout = async () => {
-        const result = await logout()
-        localStorage.removeItem('hasAccess')
-        console.log(result.message)
-        checkAccess()
-        router.refresh();
-        router.push('/');
-    }
-
-    useEffect(() => {
-        checkAccess()
-    }, [])
+export default async function VerifyAccount({ params }: { params: Promise<{ id: number }> }) {
+    const id = (await params).id
 
     return (
-        <>
-        {hasAccess ? 
-        <div style={{textAlign: 'center', marginTop: '100px'}}>
-            <p style={{textAlign: 'center', fontSize: '2em', marginBottom: '20px'}}>Welcome to your profile, {user.username}!</p>
-            <button className='button-rounded and-hover' style={{fontSize: '1.2em'}} onClick={handleLogout}>Log out</button>
-        </div> : <p>Need to login to access this page</p>}
-        </>
+        <VerifyAccountClient id={id} />
     )
 }
---
+++ src/app/(dashboard)/dashboard/dashboard.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useAuth } from "@/context/authContext";
+import { logout } from "@/lib/auth-api";
+import { useRouter } from "next/navigation"
+
+interface Props {
+    id: number;
+}
+
+export default function Dashboard({id}: Props) {
+    const {hasAccess, user, checkAccess} = useAuth();
+    const router = useRouter();
+
+    const handleLogout = async () => {
+        await logout()
+        localStorage.removeItem('hasAccess')
+        checkAccess()
+        router.refresh();
+        router.push('/');
+    }
+
+    return (
+        <>
+        {hasAccess && user ? 
+        <div style={{textAlign: 'center', marginTop: '100px'}}>
+            <p style={{textAlign: 'center', fontSize: '2em', marginBottom: '20px'}}>Welcome to your profile, {user.username}! #{id}</p>
+            <button className='button-rounded and-hover' style={{fontSize: '1.2em'}} onClick={handleLogout}>Log out</button>
+        </div> : <p>Need to login to access this page</p>}
+        </>
+    )
+}
