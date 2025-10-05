35e0f0a2038b4a1019fa0a7d149c9697dc41691d
tareqitos <contact@tareqitos.com>
2025-10-05 13:07:44 +0200

    Update main page with the title and menu cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,43 @@
-import Image from "next/image";
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
+import { Title } from "@/components/title";
+import Button from "@/components/ui/button";
+import { CardMain } from "@/components/ui/card";
+import Link from "next/link";
+
+const cards = [
+  { title: 'Resources', desc: 'Access essential materials for your study sessions.', path: 'resources', icon: "📖", shortcuts: ["Dictionary", "Reading"] },
+  { title: 'Media', desc: 'Teaching videos, vlogs, podcasts, gaming and more!', path: 'media', icon: "💾", shortcuts: ["Culture", "Podcast"] },
+  { title: 'Games', desc: 'Find and play games to enhance to practice the language!', path: 'games', icon: "🎮", shortcuts: ["Web", "Mobile"] },
+  { title: 'Software / Applications', desc: 'Useful software and applications to support your studies.', path: 'applications', icon: "💻", shortcuts: ["Flashcards", "Tools"] },
+  { title: 'And more to come', desc: 'Stay tuned for more resources and updates!', path: "", icon: "✨", shortcuts: [] }
+]
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="flex flex-col items-center px-[max(1rem,calc(50vw-600px))] border-0">
+      <div className="my-10 lg:mt-20">
+        <Title fontSize="text-6xl lg:text-8xl" logoSize="w-15 h-15 lg:w-25 lg:h-25" />
+        <p className="mt-2 px-4 py-1 text-sm bg-card-background rounded-2xl float-end">No excuses not to learn Japanese</p>
+      </div>
+      <div className=" gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {cards.map((card, index) => (
+          <Link key={index} href={card.path}>
+            <CardMain
+              icon={card.icon}
+              path={card.path}
+              title={card.title}
+              description={card.desc}
+              shortcuts={card.shortcuts}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          </Link>
+        ))}
+      </div>
+      <div className="flex gap-2 m-5">
+        <Button variant="secondary" className="transition-border cursor-pointer"><Link href="/about">About</Link></Button>
+        <Button variant="secondary" className="transition-border cursor-pointer"><Link href="https://ko-fi.com/H2H0QZVAZ">Donate</Link></Button>
+        <Button variant="secondary" className="transition-border cursor-pointer"><Link href="/updates">Updates 🌸</Link></Button>
+      </div>
     </div>
   );
 }
