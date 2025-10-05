20ca1efa4fffeb43446ef8d88fc479667b01e7e3
tareqitos <contact@tareqitos.com>
2025-10-05 13:07:13 +0200

    Create UI Card component

---
+++ components/ui/card.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+
+type CardMain = {
+    icon: string,
+    path: string,
+    title: string,
+    description: string,
+    shortcuts: string[]
+}
+
+const CardMain = ({ icon, title, description, shortcuts }: CardMain) => {
+    return (
+        <div className="flex flex-col w-full rounded-2xl p-6 lg:h-50 bg-card-background transition-border">
+            <span className="w-fit p-2 rounded-sm bg-background" >{icon}</span>
+            <h1 className="text-xl font-bold mt-2">{title}</h1>
+            <p>{description}</p>
+            {/* <Link href={`/${""}`}>{shortcuts}</Link> */}
+        </div>
+    )
+}
+
+export { CardMain }
