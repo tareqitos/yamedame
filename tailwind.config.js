4891dccd32ead373a920913e5cf0a16dedece796
tareqitos <contact@tareqitos.com>
2025-10-05 13:04:31 +0200

    Config files

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: 'var(--primary)',
+        logo: 'var(--color-logo)',
+        card: {
+            background: 'var(--card-background)'
+        },
+      },
+    },
+  },
+  plugins: [],
+}
