5c3571aa125444eef569544bdb195ca06166e20a
tareqitos <contact@tareqitos.com>
2025-10-05 21:36:22 +0200

    Remove unused shortcut prop

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -5,16 +5,14 @@ type CardMain = {
     path: string,
     title: string,
     description: string,
-    shortcuts: string[]
 }
 
-const CardMain = ({ icon, title, description, shortcuts }: CardMain) => {
+const CardMain = ({ icon, title, description }: CardMain) => {
     return (
         <div className="flex flex-col w-full rounded-2xl p-6 lg:h-50 bg-card-background transition-border">
             <span className="w-fit p-2 rounded-sm bg-background" >{icon}</span>
             <h1 className="text-xl font-bold mt-2">{title}</h1>
             <p>{description}</p>
-            {/* <Link href={`/${""}`}>{shortcuts}</Link> */}
         </div>
     )
 }
