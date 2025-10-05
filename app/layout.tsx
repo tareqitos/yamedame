49add2515714372d0e8dd3f8042fa0ba35d4114b
tareqitos <contact@tareqitos.com>
2025-10-05 13:08:28 +0200

    Added Mochiy and Nunito Google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Mochiy_Pop_P_One, Nunito } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +13,17 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const mochiyPopPOne = Mochiy_Pop_P_One({
+  variable: "--font-mochiy",
+  subsets: ["latin"],
+  weight: "400"
+})
+
+const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +38,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${mochiyPopPOne.variable} ${nunito.variable} antialiased`}
       >
+        <Header />
+        <main className="flex-1">
+
         {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
