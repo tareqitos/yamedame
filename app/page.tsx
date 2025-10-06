142d25aaff9c8ca021351dc5a47de62401ac4f02
tareqitos <contact@tareqitos.com>
2025-10-06 22:46:17 +0200

    Added the resource lists dynamic pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,27 @@
-import { Footer } from "@/components/footer";
-import { Header } from "@/components/header";
 import { Title } from "@/components/title";
 import Button from "@/components/ui/button";
 import { CardMain } from "@/components/ui/card";
+import { getResources } from "@/utils/api";
+import { getCardData } from "@/utils/constants";
 import Link from "next/link";
 
-const cards = [
-  { title: 'Resources', desc: 'Access essential materials for your study sessions.', path: 'resources', icon: "📖", shortcuts: ["Dictionary", "Reading"] },
-  { title: 'Media', desc: 'Teaching videos, vlogs, podcasts, gaming and more!', path: 'media', icon: "💾", shortcuts: ["Culture", "Podcast"] },
-  { title: 'Games', desc: 'Find and play games to enhance to practice the language!', path: 'games', icon: "🎮", shortcuts: ["Web", "Mobile"] },
-  { title: 'Software / Applications', desc: 'Useful software and applications to support your studies.', path: 'applications', icon: "💻", shortcuts: ["Flashcards", "Tools"] },
-  { title: 'And more to come', desc: 'Stay tuned for more resources and updates!', path: "", icon: "✨", shortcuts: [] }
-]
+export default async function Home() {
+  const cards = getCardData();
 
-export default function Home() {
   return (
-    <div className="flex flex-col items-center px-[max(1rem,calc(50vw-600px))] border-0">
+    <div className="flex flex-col items-center border-0">
       <div className="my-10 lg:mt-20">
         <Title fontSize="text-6xl lg:text-8xl" logoSize="w-15 h-15 lg:w-25 lg:h-25" />
         <p className="mt-2 px-4 py-1 text-sm bg-card-background rounded-2xl float-end">No excuses not to learn Japanese</p>
       </div>
       <div className=" gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {cards.map((card, index) => (
-          <Link key={index} href={card.path}>
+          <Link key={index} href={`/${card.path}`}>
             <CardMain
               icon={card.icon}
               path={card.path}
               title={card.title}
               description={card.desc}
-              shortcuts={card.shortcuts}
             />
           </Link>
         ))}
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,20 +1,39 @@
-import Link from "next/link"
+'use client'
 
-type CardMain = {
-    icon: string,
-    path: string,
-    title: string,
-    description: string,
-}
+import clsx from "clsx"
+import { Icon, IconCategory } from "./icon"
+import { CardMainProps, CardResourceProps } from "@/types/types"
 
-const CardMain = ({ icon, title, description }: CardMain) => {
+
+const CardMain = ({ icon, title, path, description }: CardMainProps) => {
     return (
-        <div className="flex flex-col w-full rounded-2xl p-6 lg:h-50 bg-card-background transition-border">
-            <span className="w-fit p-2 rounded-sm bg-background" >{icon}</span>
+        <div className="flex flex-col w-full rounded-2xl p-6 lg:h-50 bg-card-background transition">
+            <span className="w-fit p-2 rounded-sm bg-background" >
+                <Icon path={path} size={28} />
+            </span>
             <h1 className="text-xl font-bold mt-2">{title}</h1>
             <p>{description}</p>
         </div>
     )
 }
 
-export { CardMain }
+const CardResource = ({ array, className }: CardResourceProps) => {
+    return (
+        <ul className={clsx(
+            "flex flex-col gap-2 p-4 bg-card-background rounded-2xl my-6 text-primary text-lg font-semibold",
+            className
+        )}>
+            {array.map((child, index) => (
+                <li key={child.id}>
+                    <IconCategory category={child.slug} size={20} className="inline align-sub mr-2" />
+                    <a href={child.link} target="_blank" className={`flex-1 hover:text-white transition-primary`}>
+                        {child.name}
+                    </a>
+                    &nbsp;- <span className="text-white">{child.description}</span>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export { CardMain, CardResource }
