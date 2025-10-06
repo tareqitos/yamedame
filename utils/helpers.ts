d5afc20a73817c69c42c902b81e8d3deb60cdd1e
tareqitos <contact@tareqitos.com>
2025-10-06 22:46:59 +0200

    Update helpers.ts

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,24 +1,16 @@
-   export type Item = { 
-    id: string;
-    name: string; 
-    description: string, 
-    link: string, 
-    category: string; 
-    path: string;
-    image?: string
+import { Item } from "@/types/types";
+
+// Group resources by their category
+export const groupByCategory = (items: Item[]): Record<string, Item[]> => {
+    return items.reduce((groupedItems, currentItem) => {
+        const newCategory = currentItem.category;
+
+        if (!groupedItems[newCategory]) {
+            groupedItems[newCategory] = [];
+        }
+
+        groupedItems[newCategory].push(currentItem);
+
+        return groupedItems;
+    }, {} as Record<string, Item[]>);
 }
-
-   // Group resources by their category
-    export const groupByCategory = (items: Item[]): Record<string, Item[]> => {
-        return items.reduce((groupedItems, currentItem) => {
-            const newCategory = currentItem.category;
-
-            if (!groupedItems[newCategory]) {
-                groupedItems[newCategory] = [];
-            }
-
-            groupedItems[newCategory].push(currentItem);
-
-            return groupedItems;
-        }, {} as Record<string, Item[]>);
-    }
