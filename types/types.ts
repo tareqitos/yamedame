d8dd78fd8d6d7d315662b35fd93b23ae427d9c7d
tareqitos <contact@tareqitos.com>
2025-10-06 22:46:38 +0200

    Moved the types into a specific type file

---
+++ types/types.ts
@@ -0,0 +1,24 @@
+export type Item = {
+    id: string;
+    name: string;
+    description: string,
+    link: string,
+    category: string;
+    path: string;
+    slug: string;
+    image?: string
+}
+
+// Card props types
+
+export type CardMainProps = {
+    icon: string,
+    path: string,
+    title: string,
+    description: string,
+}
+
+export type CardResourceProps = {
+    array: Item[],
+    className?: string,
+}
