06aff1353184d4ab7dd9f94c1b867259a7d10521
tareqitos <contact@tareqitos.com>
2025-10-06 22:45:19 +0200

    Created icon UI component

---
+++ components/ui/icon.tsx
@@ -0,0 +1,25 @@
+import { Icons, IconsCategory } from "@/utils/constants"
+import clsx from "clsx"
+
+
+export const Icon = ({ path, size }: { path: string, size: number }) => {
+    return (
+        <>
+            {Icons.map((item, index) => (
+                item.path === path ? <item.icon key={index} size={size} color={item.iconColor} weight="regular" /> : null
+            ))}
+        </>
+    )
+}
+
+export const IconCategory = ({ category, size, className }: { category: string, size: number, className: string}) => {
+    return (
+        <>
+            {IconsCategory.map((item, index) => (
+                item.category === category ? 
+                <item.icon key={index} size={size} color={item.iconColor} weight="regular" className={clsx("max-w-30 icons", className)}/> :
+                 null
+            ))}
+        </>
+    )
+}
