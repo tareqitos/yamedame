133c31f20535bbb1c7536180c737b2480425d45b
tareqitos <contact@tareqitos.com>
2025-10-07 22:36:41 +0200

    Added blur effect on the logo

--- components/title.tsx
+++ components/title.tsx
@@ -13,6 +13,7 @@ export const Title = ({ fontSize = "text-xl", logoSize = "w-6 h-6" }: TitleProps
         <Link href="/" className={clsx("flex items-center gap-1 font-mochiy font-bold title", fontSize)}>
             <div className={clsx("relative rounded-full bg-white", logoSize)}>
                 <div className={clsx("absolute rounded-full bg-logo w-full h-full scale-101 transition-logo")}></div>
+                <div className={clsx("absolute rounded-full bg-logo w-full h-full -z-1 filter blur-3xl")}></div>
             </div>
             やめだめ
         </Link>
