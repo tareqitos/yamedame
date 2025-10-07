e800e63ff38be896188a0f74a8e6c211722c3452
tareqitos <contact@tareqitos.com>
2025-10-08 00:55:57 +0200

    Helper for converting strings into slugs

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -14,3 +14,12 @@ export const groupByCategory = (items: Item[]): Record<string, Item[]> => {
         return groupedItems;
     }, {} as Record<string, Item[]>);
 }
+
+export const convertToSlug = (text: string): string => {
+    return text
+        .toLowerCase()
+        .replace(/[&]/g, '-')
+        .replace(/[^a-z0-9-]+/g, '-') // Replace non-alphanumeric characters with hyphens
+        .replace(/--+/g, '-') // Replace multiple hyphens with a single hyphen
+        .replace(/^-+|-+$/g, ''); // Remove leading and trailing hyphens
+}
