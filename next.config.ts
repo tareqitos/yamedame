3584e8202397ebd0ae2913d5df569461939cd5ed
tareqitos <contact@tareqitos.com>
2025-10-09 22:17:40 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,22 @@
 import type { NextConfig } from "next";
+import 'dotenv/config';
 
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     optimizePackageImports: ["@phosphor-icons/react"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: `${process.env.API_IMAGE_URL}`,
+        port: '',
+        pathname: '/i/**',
+      },
+    ],
+    qualities: [1, 25, 50, 75, 80, 100],
+
   },
 };
 
