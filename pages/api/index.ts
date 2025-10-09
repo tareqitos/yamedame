96f112e7258368f8e78873e2da5dc54a19fd1387
tareqitos <contact@tareqitos.com>
2025-10-09 22:17:32 +0200

    Removed unused file

--- pages/api/index.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { initDatabase } from '../../scripts/init';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await initDatabase(); // Ensure DB and tables exist
-  res.status(200).json({ message: 'DB is ready!', });
-}
