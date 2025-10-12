2383ff542cf79c0a98a8fe76e002c3b17d3dab90
tareqitos <contact@tareqitos.com>
2025-10-12 22:40:11 +0200

    Added Reading category

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,10 +1,11 @@
-import { BookOpenTextIcon, GameControllerIcon, DesktopIcon, TranslateIcon, VideoCameraIcon, SparkleIcon, FlowerTulipIcon, DeviceMobileIcon, BrowserIcon, CardsThreeIcon, KanbanIcon, BooksIcon, ToolboxIcon, BowlSteamIcon, GlobeHemisphereEastIcon, GrainsIcon, TelevisionIcon, JoystickIcon, CassetteTapeIcon, YoutubeLogoIcon, SpotifyLogoIcon } from "@phosphor-icons/react/dist/ssr";
+import { BookOpenTextIcon, GameControllerIcon, DesktopIcon, TranslateIcon, VideoCameraIcon, SparkleIcon, FlowerTulipIcon, DeviceMobileIcon, BrowserIcon, CardsThreeIcon, KanbanIcon, BooksIcon, ToolboxIcon, BowlSteamIcon, GlobeHemisphereEastIcon, GrainsIcon, TelevisionIcon, JoystickIcon, CassetteTapeIcon, YoutubeLogoIcon, SpotifyLogoIcon, BookIcon } from "@phosphor-icons/react/dist/ssr";
 
 // Using Phosphor Icons
 export const Icons = [
   { path: 'dictionaries', icon: BooksIcon, iconColor: "#23AE5B" },
   { path: 'vocabulary', icon: TranslateIcon, iconColor: "#426BFE" },
   { path: 'grammar', icon: BookOpenTextIcon, iconColor: "#bdc3c7" },
+  { path: 'reading', icon: BookIcon, iconColor: "#67cdd1" },
   { path: 'media', icon: VideoCameraIcon, iconColor: "#D084D6" },
   { path: 'games', icon: GameControllerIcon, iconColor: "#e67e22" },
   { path: 'applications', icon: DesktopIcon, iconColor: "#de6878" },
@@ -17,6 +18,7 @@ export const IconsCategory = [
   { category: 'dictionaries-apps', icon: BooksIcon, iconColor: "#de6878" },
   { category: 'vocabulary-kanji', icon: TranslateIcon, iconColor: "#426BFE" },
   { category: 'grammar', icon: BookOpenTextIcon, iconColor: "#bdc3c7" },
+  { category: 'reading', icon: BookIcon, iconColor: "#67cdd1" },
   { category: 'kana', icon: FlowerTulipIcon, iconColor: "#d6408bff" },
   { category: 'mobile', icon: DeviceMobileIcon, iconColor: "#e67e22" },
   { category: 'web', icon: BrowserIcon, iconColor: "#e67e22" },
@@ -42,6 +44,7 @@ export const getCardData = () => {
     { title: 'Dictionaries', desc: 'Essential dictionaries and reference tools for Japanese learning.', path: 'dictionaries', icon: "📚" },
     { title: 'Vocabulary', desc: 'Resources to help you build and expand your Japanese vocabulary.', path: 'vocabulary', icon: "📝" },
     { title: 'Grammar', desc: 'Master Japanese grammar rules, patterns, and sentence structures.', path: 'grammar', icon: "📖" },
+    { title: 'Reading', desc: 'Practice reading skills with various texts, articles, and stories.', path: 'reading', icon: "📚" },
     { title: 'Media', desc: 'Teaching videos, vlogs, podcasts, gaming and more!', path: 'media', icon: "💾" },
     { title: 'Games', desc: 'Find and play games to enhance to practice the language!', path: 'games', icon: "🎮" },
     { title: 'Software / Applications', desc: 'Useful software and applications to support your studies.', path: 'applications', icon: "💻" },
