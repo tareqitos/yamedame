c0486a642ee45894d0861f71e293d2ad7003cc45
tareqitos <contact@tareqitos.com>
2025-10-26 01:47:46 +0200

    Update resource API endpoint

--- utils/api.ts
+++ utils/api.ts
@@ -1,14 +1,25 @@
 import { cache } from 'react'
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
 
 export const getResources = cache(async () => {
-    const API_URL = process.env.API_URL || 'http://localhost:3000'
 
     try {
-        const response = await fetch(`${API_URL}/api`)
+        const response = await fetch(`${API_URL}/resources`, {
+            headers: {
+                'x-api-key': process.env.NEXT_PUBLIC_API_KEY!
+            },
+            cache: 'no-store',
+            next: { revalidate: 0 },
+        })
+
+        if (!response.ok) {
+            console.error(`Get resources failed! : ${response.status}`)
+        }
+
         const result = await response.json()
-        return result
+        return Array.isArray(result) ? result : [];
     } catch (error) {
         console.error("Failed to fetch resources: ", error)
-        return null
+        return [];
     }
 })
