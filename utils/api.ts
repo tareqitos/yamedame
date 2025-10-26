d65380082e6071a4243b32c611ad0d39c460f7f1
tareqitos <contact@tareqitos.com>
2025-10-26 02:06:31 +0200

    Update api.ts

--- utils/api.ts
+++ utils/api.ts
@@ -4,13 +4,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
 export const getResources = cache(async () => {
 
     try {
-        const response = await fetch(`${API_URL}/resources`, {
-            headers: {
-                'x-api-key': process.env.NEXT_PUBLIC_API_KEY!
-            },
-            cache: 'no-store',
-            next: { revalidate: 0 },
-        })
+        const response = await fetch(`${API_URL}/resources`)
 
         if (!response.ok) {
             console.error(`Get resources failed! : ${response.status}`)
