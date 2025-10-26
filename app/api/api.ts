100240ee629c85deb940c9b8de25a4401a7f7085
tareqitos <contact@tareqitos.com>
2025-10-26 12:51:15 +0100

    Fixed API error handling

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,10 +1,16 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export async function getResources() {
-    const response = await fetch(`${API_URL}/resources`);
-    if (response.status !== 200) {
-        return { response, result: null }
+    try {
+        const response = await fetch(`${API_URL}/resources`);
+        if (!response.ok) {
+            console.error(`Get resources failed! : ${response.status}`);
+            return [];
+        }
+        const result = await response.json();
+        return result || [];
+    } catch (error) {
+        console.error("Failed to fetch resources: ", error);
+        return [];
     }
-    const result = await response.json();
-    return result || []
 }
